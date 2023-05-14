38f7a543078fe02cb6ff28fd8adad252fdf5f940
ledung2509 <ledungb12509@gmail.com>
2023-05-14 22:45:34 +0700

    initial commit

---
+++ src/bt18/Main.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bt18;
+import java.util.*;
+/**
+ *
+ * @author ADMIN
+ */
+//class Shape{
+//    protected int a;
+//    
+//    protected Shape(){
+//        a=10;
+//    }
+//}
+
+public class Main {
+    static void foo(String x){
+        System.out.println("String");
+    } 
+    static void foo(StringBuffer y){
+        System.out.println("StringBuffer");
+    }
+    static void foo(Integer z){
+        System.out.println("Integer");
+    }
+    public static void main(String[] args) {
+//        TreeSet<Integer> t = new TreeSet<Integer>();
+//        t.add(3);
+//        t.add(2);
+//        t.add(4);
+//        t.add(2);
+//        t.add(5);
+//        t.add(2);
+//        for(int x:t)
+//            System.out.println(x + " ");
+//            System.out.println( );
+//        Integer i = null;
+//        if(i!= null & i.intValue()==5){
+//            System.out.println("Value is 5");
+//        }
+       
+        System.out.println();
+   }       
+}
